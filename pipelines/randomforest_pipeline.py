28caed03b82b8ca5718276ed67e48ede64689a60
Andrew Stinson <a.c.stinson119@gmail.com>
2026-08-28 10:02:42 -0400

    final model settings

--- pipelines/randomforest_pipeline.py
+++ pipelines/randomforest_pipeline.py
@@ -4,10 +4,10 @@ from sklearn.ensemble import RandomForestClassifier
 def build_model():
     model = RandomForestClassifier(
         n_estimators=300,
-        max_depth=40,
+        max_depth=None,
         min_samples_split=2,
-        min_samples_leaf=4,
-        max_features='log2',
+        min_samples_leaf=2,
+        max_features=None,
         class_weight='balanced'
     )
 
